679765d2d67d2b84a94a8d119dc61d74777caeb7
VINICIUS BONATTI BENNER <vinicius_benner@estudante.sesisenai.org.br>
2022-09-14 21:53:29 -0300

    nao encontrado

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const nomeApp = process.env.npm_package_name;
+const app = express();
+
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+
+app.get('/*', (req, res) => {
+res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+});
+
+app.listen(process.env.PORT || 8080);
+console.log("Tá rodando na porta 8080 mano, só pra avisar")
