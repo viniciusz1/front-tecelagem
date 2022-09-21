41c9b61095b232a255f606c626487ff909a3836f
viniciusz1 <vinicius101313@gmail.com>
2022-09-21 14:05:42 -0300

    historico produção

--- src/app/historico-producao/historico-producao.component.ts
+++ src/app/historico-producao/historico-producao.component.ts
@@ -1,17 +1,121 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
+import { Router } from '@angular/router';
+
+import { ClienteService } from '../service/cliente.service';
+
+import { FuncionarioService } from '../service/funcionario.service';
+
+import { MaquinaService } from '../service/maquina.service';
+
+import { ProducaoService } from '../service/producao.service';
+
+
+
+
+interface Producao {
+
+  id: string;
+
+  pesoRolo: number;
+
+  defeito: string;
+
+  clienteId: string;
+
+  funcionarioId: string;
+
+  maquinaId: string;
+
+  status: string;
+
+  data: string;
+
+}
+
+
+
+
 @Component({
+
   selector: 'app-historico-producao',
+
   templateUrl: './historico-producao.component.html',
-  styleUrls: ['./historico-producao.component.css']
+
+  styleUrls: ['./historico-producao.component.css'],
+
 })
+
 export class HistoricoProducaoComponent implements OnInit {
-    @Output() statusModalDetalhes = new EventEmitter;
-  constructor() { }
-    close(){
-        this.statusModalDetalhes.emit()
-    }
+
+  @Output() statusModalDetalhes = new EventEmitter();
+
+  constructor(
+
+    private router: Router,
+
+    private producaoService: ProducaoService,
+
+    private funcionarioService: FuncionarioService,
+
+    private clienteService: ClienteService,
+
+    private maquinaService: MaquinaService
+
+  ) {}
+
+
+
+
+  producao: Producao[] = [];
+
+  listaProducoes = [{}];
+
+
+
+
+  clienteNome = '';
+
+  funcionario = '';
+
+  maquinaNome = '';
+
+  funcionarioNome = '';
+
+  cliente = [{}];
+
+
+
+
+  close() {
+
+    this.statusModalDetalhes.emit();
+
+  }
+
   ngOnInit(): void {
+
+    this.producao = [];
+
+    this.clienteNome = '';
+
+    this.maquinaNome = '';
+
+    this.funcionarioNome = '';
+
+    this.cliente = [];
+
+    this.listaProducoes = [];
+
+
+
+
+    this.producaoService.buscarProducao().subscribe((e) => {
+
+      this.producao = Object.values(e);
+
+    });
+
   }
 
 }
